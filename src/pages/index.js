fdf418ae0497a9636d4c84f9eae0f43e62b6578a
Niall Whelan <niallwhelan2010@hotmail.com>
2021-06-29 14:37:15 +0100

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from 'react';
+import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
+
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <p>This is my second child</p>
+      <StaticImage
+        width={700}
+        alt="Dog in Crab Costume"
+        src='https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg'
+      />
     </Layout>
   )
 }
